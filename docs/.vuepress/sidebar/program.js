b9c6d3c3887b9048542061439274c1b3d59374b1
yinhaiying <18840842571@163.com>
2020-04-10 12:30:14 +0800

    修改错误

--- docs/.vuepress/sidebar/program.js
+++ docs/.vuepress/sidebar/program.js
@@ -9,14 +9,6 @@ module.exports = {
           ["./vue/vue001","【001】从零开始搭建vue"],
         ]
       },
-      {
-        "name": "React",
-        "title": "React",
-        "collabsable": false,
-        "children": [
-          ["react001","【001】从零开始搭建react"],
-        ]
-      },
     ]
 }
 
