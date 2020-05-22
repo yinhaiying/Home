a9815774dbbd0bd0cd8ab774bba2f2b443afba10
yinhaiying <yinhaiying@58.com>
2020-05-22 18:28:49 +0800

    socket.io

--- docs/.vuepress/sidebar/node.js
+++ docs/.vuepress/sidebar/node.js
@@ -6,6 +6,7 @@ module.exports = {
           "collabsable": false,
           "children": [
             ["node001","【001】Node实现自定义命令行工具"],
+            ["node002","【002】socket.io初体验之简易聊天室"],
           ]
         },
       ]
