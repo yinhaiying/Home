adcae80e6fee93a7c6de26a24f6293aec613c8fa
yinhaiying <yinhaiying@58.com>
2020-05-31 19:26:19 +0800

    MySQL注意事项

--- docs/.vuepress/sidebar/mysql.js
+++ docs/.vuepress/sidebar/mysql.js
@@ -6,6 +6,7 @@ module.exports = {
           "collabsable": false,
           "children": [
             ["./mysql001","【001】mysql入门（一）库表操作"],
+            ["./mysql002","【002】mysql的使用注意事项"],
           ]
         },
       ]
