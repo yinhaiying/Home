b86146bbd991112410e204fe1eda1dec2041d6ad
yinhaiying <yinhaiying@58.com>
2020-06-19 19:49:46 +0800

    node4

--- docs/.vuepress/sidebar/node.js
+++ docs/.vuepress/sidebar/node.js
@@ -8,6 +8,7 @@ module.exports = {
             ["node001","【001】Node实现自定义命令行工具"],
             ["node002","【002】socket.io初体验之简易聊天室"],
             ["node003","【003】Express核心之中间件"],
+            ["node004","【004】Express基础"],
           ]
         },
       ]
