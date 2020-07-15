ca1cbe35cee302259330f47f7454445707bb9c81
yinhaiying <yinhaiying@58.com>
2020-07-15 17:56:19 +0800

    vue3.0初体验安装

--- docs/.vuepress/sidebar/vue.js
+++ docs/.vuepress/sidebar/vue.js
@@ -1,18 +1,17 @@
 module.exports = {
-    '/vue/':[
-        // ['','目录'],
-        {
-          "name": "Vue",
-          "title": "Vue",
-          "collabsable": false,
-          "children": [
-            // ["", "目录"],
-            ["vue001","【001】Vue中过渡和动画"],
-            ["vue002","【002】Vue的组件化"],
-            ["vue003","【003】Vue.extend动态创建实例"],
-          ]
-        }
-      ]
-  }
-  
-  
+  "/vue/": [
+    // ['','目录'],
+    {
+      name: "Vue",
+      title: "Vue",
+      collabsable: false,
+      children: [
+        // ["", "目录"],
+        ["vue001", "【001】Vue中过渡和动画"],
+        ["vue002", "【002】Vue的组件化"],
+        ["vue003", "【003】Vue.extend动态创建实例"],
+        ["vue004", "【004】Vue3.0 CompositionAPI 初体验"],
+      ],
+    },
+  ],
+};
